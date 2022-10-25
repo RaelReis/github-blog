c62f74905743cfc37a6d437b9cd8d8bdb1aa3e74
RaelReis <rafael.reeis03@gmail.com>
2022-10-25 20:56:56 -0300

    Up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="w-full h-[296px] bg-[url(/assets/images/header-bg.png)] bg-cover bg-no-repeat bg-center">
       <div className="h-[98px] container flex justify-center mt-16">
-        <Image src="/assets/images/logo.svg" alt="" width={148} height={98} priority/>
+        <Image src="/assets/images/logo.svg" alt="" width={148} height={98} layout="responsive" priority />
       </div>
     </header>
   );
