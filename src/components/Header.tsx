e711678aa6d27269a6b8d1d62daad5c54e034dd5
RaelReis <rafael.reeis03@gmail.com>
2022-10-25 20:58:58 -0300

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Header() {
   return (
     <header className="w-full h-[296px] bg-[url(/assets/images/header-bg.png)] bg-cover bg-no-repeat bg-center">
-      <div className="h-[98px] container flex justify-center mt-16">
+      <div className="h-[98px] w-[148] container flex justify-center mt-16">
         <Image src="/assets/images/logo.svg" alt="" width={148} height={98} layout="responsive" priority />
       </div>
     </header>
